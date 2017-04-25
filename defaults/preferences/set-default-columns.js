c1461b1a17eeb49fd09dd3ff0caf2e6c104016d9
YUKI Hiroshi <shimoda@clear-code.com>
2017-04-25 13:20:37 +0900

    Set corresponder visible by default

--- defaults/preferences/set-default-columns.js
+++ defaults/preferences/set-default-columns.js
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("extensions.set-default-columns@clear-code.com.columns", "threadCol,attachmentCol,flaggedCol,subjectCol,unreadButtonColHeader,senderCol,recipientCol,junkStatusCol,dateCol,locationCol");
+pref("extensions.set-default-columns@clear-code.com.columns", "threadCol,attachmentCol,flaggedCol,subjectCol,unreadButtonColHeader,correspondentCol,junkStatusCol,dateCol,locationCol");
 
